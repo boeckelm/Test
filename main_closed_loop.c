bc895d2626ef80d3fd647816d80eea4a0aa51265
boeckelm <61057836+boeckelm@users.noreply.github.com>
2022-06-23 23:54:32 +0200

    Update main_closed_loop.c

--- main_closed_loop.c
+++ main_closed_loop.c
@@ -24,21 +24,19 @@
 #define KI        3
 #define TA       10
 #define SCALE  1000
+#define PWM_MAX 4000
 
 
-uint16_t Adc_Convert( uint8_t i);
-uint16_t absolut16 (int16_t val);
-void linearMapToPWM (uint16_t min, uint16_t max, uint16_t value, TIMER_TypeDef Timer);
 int16_t linearMap (int16_t value, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max);
-void setDirection(uint8_t val); //Sollte aus ISR aufrufbar sein, hier irrelevent
-int32_t Limit (int16_t value, int16_t min, int16_t max);
-
+uint16_t absolut16 (int16_t val);
+void setDirection(uint8_t val);                                             //Sollte aus ISR aufrufbar sein, hier irrelevent
 uint16_t Adc_Convert( uint8_t i);
-uint16_t AdcToVolt (uint16_t CW);
-int16_t PiRegler( int16_t nsoll, int16_t nist );
+uint16_t AdcToVolt (uint16_t CW);                                           //Funktion CW->Spannung/Strom
+int16_t PiRegler( int16_t nsoll, int16_t nist );                            //PI-Regler für ISR
+int32_t Limit (int32_t value, int32_t min, int32_t max);                    //Limit-Funktion
 
-void encoderISR(void); //Funktion sollte in zwingend in ITCM-RAM (über AHB) @STM32H7
-void controlISR(void);
+void encoderISR(void); //Für Encoder -> RPM Bestimmung
+void controlISR(void); //Für Regelkreis
 
 
 // Folgende Studierende haben den Versuch zusammen erstellt und abgegeben:
@@ -52,68 +50,65 @@ void main(void) {
     System_Init();
     Display_Init(); //Display initialisieren
 
-    Pin_Init(DIR_0, PIN_GPIO_OUT); //LED D1 bis D4 als Ausgang
+    //DIR-Outputs
+    Pin_Init(DIR_0, PIN_GPIO_OUT); //DIR
     Pin_Init(DIR_1, PIN_GPIO_OUT);
     Pin_SetOutput(DIR_0, 1); //Am Anfang lieber mal ausschalten bevor undefine state auftritt
     Pin_SetOutput(DIR_1, 1);
 
-
+    //ADC-Config
     Pin_Init(ADC_MUX_CTRL_A, PIN_GPIO_OUT); //Pins zur Ansteuerung des ADC-Muxers konfigurieren (Output)
     Pin_Init(ADC_MUX_CTRL_B, PIN_GPIO_OUT);
     Pin_Init(ADC_MUX_CTRL_C, PIN_GPIO_OUT);
     Pin_Init(ADC_CH1_IN_PIN, PIN_GPIO_IN);   //ADC_CH1_IN_PIN als Eingang
     Adc_Init();
 
-    Timer_InitPwm(TIMER_3A, 3999); //PWM für Motor, 20kHz
+    //PWM für Motor
+    Timer_InitPwm(TIMER_3A, PWM_MAX - 1); //PWM für Motor, 20kHz
     Pin_Init(PWM_MOTOR, PIN_TIMER_OUT);
 
+    //Timer für diskreten Regler
     Timer_Init(TIMER_1A, 799999); //Regler-Interrupt, 100Hz, M = 799.999
-    Timer_EnableOverflowIrq(TIMER_1A);
-    Timer_Start(TIMER_1A);
-    Isr_Register(IRQ_TIMER_1A, controlISR);
+    Timer_EnableOverflowIrq(TIMER_1A); //Overflow-IRQ aktivieren
+    Timer_Start(TIMER_1A);  //Timer starten
+    Isr_Register(IRQ_TIMER_1A, controlISR); //ISR registrieren
 
+    //Capture für Motordrehzahl
+    Timer_InitCapture(TIMER_2A); //Modul aktivieren
+    Pin_Init(ENCODER_A, PIN_TIMER_IN);  //Eingang definieren
+    Timer_EnableCaptureIrq(TIMER_2A);   //IRQ wenn Rising Edge
+    Isr_Register(IRQ_TIMER_2A, encoderISR); //ISR regstrieren
+    Pin_Init(ENCODER_B, PIN_GPIO_IN);   //Drehrichtung
 
-    Timer_InitCapture(TIMER_2A);
-    Pin_Init(ENCODER_A, PIN_TIMER_IN);
-    Timer_EnableCaptureIrq(TIMER_2A);
-    Isr_Register(IRQ_TIMER_2A, encoderISR);
-    Pin_Init(ENCODER_B, PIN_GPIO_IN);
-
+    //ISRs aktiviern
     Isr_EnableAll();
 
+    //Für Visualisierung
     Uart_Init(UART_0, 115200);
 
-
-    uint16_t CWPoti, CWIL, CWIR;    //Variablen zum Zwischenspeichern von Werten
+    //Zwischenspeicher
+    uint16_t CWPoti, IL, IR;    //Variablen zum Zwischenspeichern von Werten
 
        while(1) {
 
         CWPoti = Adc_Convert(ADC_MUX_IN_POTI);  //Analogwert von Poti auslesen
-        CWIL = Adc_Convert(ADC_MUX_IN_IL);
-        CWIR = Adc_Convert(ADC_MUX_IN_IR);
+        IL = AdcToVolt(Adc_Convert(ADC_MUX_IN_IL));
+        IR = AdcToVolt(Adc_Convert(ADC_MUX_IN_IR));
 
-        NSOLL = linearMap((int16_t)CWPoti, 0, 4095, -6000, 6000);//CWPoti [0;4095] auf das gewünschte Format mappen [0; 6000]
+        NSOLL = linearMap((int16_t)CWPoti, 0, 4095, -6000, 6000);//CWPoti [0;4095] auf das gewünschte Format mappen [-6000; 6000]
 
-        Display_Printf(0, 0, "CWPoti %04d", CWPoti);
-        Display_Printf(1, 0, "nsoll: %05d", NSOLL);
-        Display_Printf(2, 0, "IL: %05d", AdcToVolt(CWIL));
-        Display_Printf(3, 0, "IR: %05d", AdcToVolt(CWIR));
-        Display_Printf(4, 0, "nist: %05d", RPM);
+        Display_Printf(0, 0, "REGELUNG");
+        Display_Printf(1, 0, "CWPoti %04d", CWPoti);
+        Display_Printf(2, 0, "IL: %04d", CWIL);
+        Display_Printf(3, 0, "IR: %04d", CWIR);
+        Display_Printf(4, 0, "nsoll: %05d RPM", NSOLL);
+        Display_Printf(5, 0, "nist:  %05d RPM", RPM);
 
-        printf("%d,%d,%d,%d\r\n", NSOLL,RPM,AdcToVolt(CWIL),AdcToVolt(CWIR));
+        printf("%d,%d,%d,%d\r\n", NSOLL, RPM, IL, IR); //Für Plot
         DelayMs(100);
         Display_Update();
 
     }
-}
-
-void linearMapToPWM (uint16_t min, uint16_t max, uint16_t value, TIMER_TypeDef Timer){
-
-    uint16_t P = (value-min)*4000 / (max-min); //Linear mapping auf PWM-Bereich TODO:Timer-Max aus struct holen
-
-    Timer_SetPwmValue(Timer, P);
-
-    return;
 }
 
 int16_t linearMap (int16_t value, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max){
@@ -132,8 +127,9 @@ uint16_t absolut16 (int16_t val){
 
 void setDirection(uint8_t val){ //0: vorwärts, im Uhrzeigersinn, 1: rückwärts
 
-    Pin_SetOutput(DIR_0, val);
-    Pin_SetOutput(DIR_1, !val); //eig. schlecht, in 2 monaten hab ich keine Ahnung mehr was ich hier mache
+    Pin_SetOutput(DIR_0, val); //0000(!VAL)(VAL)00
+    Pin_SetOutput(DIR_1, !val);
+
     return;
 
 }
@@ -189,18 +185,19 @@ void controlISR(void){
 
 
 int16_t PiRegler( int16_t nsoll, int16_t nist ) {
+    static int16_t lim = 4000*SCALE/(KI*TA);
     int16_t e, y;
     e = nsoll - nist;
     esum += e;
-    e = Limit(e, -4000*SCALE/(KI*TA), 4000*SCALE/(KI*TA));
+    e = Limit(e, -lim, lim);
 
     y = ((int32_t)e*KP + esum*KI*TA)/SCALE; // Zeitdiskreter PI-Regler
 
 
-    return Limit(y, -4000, 4000 );// Begrenzung fuer y
+    return Limit(y, -PWM_MAX, PWM_MAX );// Begrenzung fuer y
 }
 
-int32_t Limit (int16_t value, int16_t min, int16_t max){
+int32_t Limit (int32_t value, int32_t min, int32_t max){
 
     if(value < min) return min;
     else if(value > max) return max;
